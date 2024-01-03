b858fb5e237f93dc7410210934766f65da547810
heybadrinath <ram.r@kalvium.community>
2024-01-03 18:25:03 +0530

    completed the react app components

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
   return {
+
+    server: {
+      port: 6969,
+    },
     build: {
       outDir: 'build',
     },
